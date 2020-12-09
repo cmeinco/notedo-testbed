dfbffd2d9e99d090dc0ca915e2f5cd46bcaf2fe2
cmeinco <trent@appsec.wtf>
2020-12-09 16:17:38 -0600

    removed open prompt

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 robot --variable BROWSER:Firefox --outputdir results --report report-firefox.html --log log-firefox.log.html --output output-firefox.xml  --noncritical bugs notedo.robot
-open results/report-firefox.html
+# open results/report-firefox.html
 
 # robot --variable BROWSER:Chrome --outputdir results --report report-chrome.html --log log-chrome.log.html --output output-chrome.xml  --noncritical bugs notedo.robot
 # open results/report-chrome.html
