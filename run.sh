9a16425b8b5aa9e5a848aef6db6337c59b0865c4
cmeinco <trent@appsec.wtf>
2020-12-09 21:31:09 -0600

    testing

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+firefox --headless http://localhost:8081 --screenshot 
 
 source venv/bin/activate
-robot --variable BROWSER:headlesschrome --outputdir results --report report.html --log log.html --output output.xml  --noncritical bugs notedo.robot
+robot --variable BROWSER:headlessfirefox --outputdir results --report report.html --log log.html --output output.xml  --noncritical bugs notedo.robot
 # open results/report-firefox.html
 
 # robot --variable BROWSER:Chrome --outputdir results --report report-chrome.html --log log-chrome.log.html --output output-chrome.xml  --noncritical bugs notedo.robot
